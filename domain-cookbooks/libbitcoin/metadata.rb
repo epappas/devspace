1c5edd83b5c80e407f1bebef72c05b086db8468c
Evangelos Pappas <epappas@evalonlabs.com>
2016-03-05 11:11:33 +0000

    install libbitcoin

---
+++ domain-cookbooks/libbitcoin/metadata.rb
@@ -0,0 +1,4 @@
+name 'libbitcoin'
+description 'Installs/Configures libbitcoin'
+long_description 'Installs/Configures libbitcoin'
+version '0.1.0'
---
+++ domain-cookbooks/libbitcoin/recipes/default.rb
@@ -0,0 +1,33 @@
+
+directory '/opt/libbitcoin' do
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+    not_if { File.exist?('/opt/libbitcoin') }
+end
+
+# git "/opt/libbitcoin" do
+#    repository "https://github.com/libbitcoin/libbitcoin-explorer.git"
+#    reference "v2.2.0"
+#    action :sync
+#    action :create_if_missing
+#end
+
+remote_file '/opt/libbitcoin/install.sh' do
+  source 'https://raw.githubusercontent.com/libbitcoin/libbitcoin-explorer/version2/install.sh'
+  action :create_if_missing
+end
+
+#execute 'libbitcoin update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 50' do
+#  command 'update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 50'
+#  cwd '/opt/libbitcoin'
+#  not_if { File.exist?('/usr/local/bin/bx') }
+#end
+
+execute 'libbitcoin ./install.sh' do
+  command 'chmod +x install.sh && ./install.sh'
+  cwd '/opt/libbitcoin'
+  not_if { File.exist?('/usr/local/bin/bx') }
+end
+
