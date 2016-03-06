cfb150ee116303f83ceb4a5d30f93af910710611
Evangelos Pappas <epappas@evalonlabs.com>
2016-03-06 08:01:20 +0000

    fix libbitcoin

--- domain-cookbooks/libbitcoin/recipes/default.rb
+++ domain-cookbooks/libbitcoin/recipes/default.rb
@@ -14,9 +14,17 @@ end
 #    action :create_if_missing
 #end
 
-remote_file '/opt/libbitcoin/install.sh' do
-  source 'https://raw.githubusercontent.com/libbitcoin/libbitcoin-explorer/version2/install.sh'
-  action :create_if_missing
+# remote_file '/opt/libbitcoin/install.sh' do
+#   source 'https://raw.githubusercontent.com/libbitcoin/libbitcoin-explorer/version2/install.sh'
+#   action :create_if_missing
+# end
+
+template "/opt/libbitcoin/install.sh" do
+    source "install.sh.erb"
+    owner "root"
+    group "root"
+    mode 0755
+    not_if { File.exist?('/opt/libbitcoin/install.sh') }
 end
 
 #execute 'libbitcoin update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 50' do
